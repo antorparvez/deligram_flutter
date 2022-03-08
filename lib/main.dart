20a14654721ad4c8d8be3e060f6ce659523a1fc5
antor <antorparvez@gmail.com>
2022-03-08 14:41:38 +0600

    list using flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,19 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+
+
+List<int> list = [];
+
 class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    for(int i=0; i<= 10 ; i++){
+      list.add(i);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,20 +31,13 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(body: DeligramTestApp()),
+      // Call the Custom widget here///
+      home: const Scaffold(body: ListViewBuilder()),
     );
   }
 }
 
-class DeligramTestApp extends StatelessWidget {
-  const DeligramTestApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const FlutterColumnRow();
-  }
-}
-
+///ROWS AND COLUMNS STATELESS WIDGET
 class FlutterColumnRow extends StatelessWidget {
   const FlutterColumnRow({Key? key}) : super(key: key);
 
@@ -62,6 +67,124 @@ class FlutterColumnRow extends StatelessWidget {
     );
   }
 }
+
+
+
+///LIST WITH TILES STATEFUL WIDGET
+
+class FlutterListTiles extends StatefulWidget {
+  const FlutterListTiles({Key? key}) : super(key: key);
+
+  @override
+  _FlutterListTilesState createState() => _FlutterListTilesState();
+}
+
+class _FlutterListTilesState extends State<FlutterListTiles> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("sdfadfsadsf"),
+          tileColor: Colors.green,
+          trailing: Icon(
+              Icons.arrow_back
+          ),
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow
+            ),
+          ),
+        ),
+        ListTile(
+          title: Text("sdfadfsadsf"),
+          tileColor: Colors.green,
+          trailing: Icon(
+              Icons.arrow_back
+          ),
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow
+            ),
+          ),
+        ),
+        ListTile(
+          title: Text("sdfadfsadsf"),
+          tileColor: Colors.green,
+          trailing: Icon(
+              Icons.arrow_back
+          ),
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow
+            ),
+          ),
+        ),
+        ListTile(
+          title: Text("sdfadfsadsf"),
+          tileColor: Colors.green,
+          trailing: Icon(
+              Icons.arrow_back
+          ),
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ListViewBuilder extends StatefulWidget {
+  const ListViewBuilder({Key? key}) : super(key: key);
+
+  @override
+  _ListViewBuilderState createState() => _ListViewBuilderState();
+}
+
+///LISTVIEW WITH BUILDER STATEFUL WIDGET
+class _ListViewBuilderState extends State<ListViewBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext cnx ,int index){
+          return   ListTile(
+            title: Text("List Item ${list[index]}"),
+            tileColor: Colors.green,
+            trailing: Icon(
+                Icons.arrow_back
+            ),
+            leading: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.yellow
+              ),
+            ),
+          );
+        },
+
+    );
+  }
+}
+
+
 
 
 
