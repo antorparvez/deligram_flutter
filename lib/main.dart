331d734866f55186bc3858833743c68855d85503
antor <antorparvez@gmail.com>
2022-03-09 10:52:43 +0600

    Nested list

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,17 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-
-
 List<int> list = [];
+List<int> list2 = [];
 
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
-    for(int i=0; i<= 10 ; i++){
+    for (int i = 0; i <= 10; i++) {
       list.add(i);
+      for (int j = 50; j <= 60; j++) {
+        list2.add(j);
+      }
     }
     super.initState();
   }
@@ -32,7 +34,7 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       // Call the Custom widget here///
-      home: const Scaffold(body: ListViewBuilder()),
+      home: const Scaffold(body: NestedListView()),
     );
   }
 }
@@ -46,29 +48,18 @@ class FlutterColumnRow extends StatelessWidget {
     return Column(
       children: [
         Row(
-          children: const [
-            Text("data"),
-            Text("data2")
-          ],
+          children: const [Text("data"), Text("data2")],
         ),
         Row(
-          children: const [
-            Text("data"),
-            Text("data2")
-          ],
+          children: const [Text("data"), Text("data2")],
         ),
         Row(
-          children: const [
-            Text("data"),
-            Text("data2")
-          ],
+          children: const [Text("data"), Text("data2")],
         )
       ],
     );
   }
 }
-
-
 
 ///LIST WITH TILES STATEFUL WIDGET
 
@@ -87,61 +78,45 @@ class _FlutterListTilesState extends State<FlutterListTiles> {
         ListTile(
           title: Text("sdfadfsadsf"),
           tileColor: Colors.green,
-          trailing: Icon(
-              Icons.arrow_back
-          ),
+          trailing: Icon(Icons.arrow_back),
           leading: Container(
             height: 50,
             width: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.yellow
-            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
           ),
         ),
         ListTile(
           title: Text("sdfadfsadsf"),
           tileColor: Colors.green,
-          trailing: Icon(
-              Icons.arrow_back
-          ),
+          trailing: Icon(Icons.arrow_back),
           leading: Container(
             height: 50,
             width: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.yellow
-            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
           ),
         ),
         ListTile(
           title: Text("sdfadfsadsf"),
           tileColor: Colors.green,
-          trailing: Icon(
-              Icons.arrow_back
-          ),
+          trailing: Icon(Icons.arrow_back),
           leading: Container(
             height: 50,
             width: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.yellow
-            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
           ),
         ),
         ListTile(
           title: Text("sdfadfsadsf"),
           tileColor: Colors.green,
-          trailing: Icon(
-              Icons.arrow_back
-          ),
+          trailing: Icon(Icons.arrow_back),
           leading: Container(
             height: 50,
             width: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.yellow
-            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
           ),
         ),
       ],
@@ -161,31 +136,75 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (BuildContext cnx ,int index){
-          return   ListTile(
-            title: Text("List Item ${list[index]}"),
-            tileColor: Colors.green,
-            trailing: Icon(
-                Icons.arrow_back
-            ),
-            leading: Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.yellow
-              ),
-            ),
-          );
-        },
-
+      itemCount: list.length,
+      itemBuilder: (BuildContext cnx, int index) {
+        return ListTile(
+          title: Text("List Item ${list[index]}"),
+          tileColor: Colors.green,
+          trailing: Icon(Icons.arrow_back),
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
+          ),
+        );
+      },
     );
   }
 }
 
+class NestedListView extends StatefulWidget {
+  const NestedListView({Key? key}) : super(key: key);
 
+  @override
+  _NestedListViewState createState() => _NestedListViewState();
+}
 
-
-
-
+class _NestedListViewState extends State<NestedListView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        separatorBuilder: (context, item) => const Divider(
+              color: Colors.black,
+            ),
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int item) {
+          return Column(
+            children: [
+              ListTile(
+                title: Text("List 1 position ${list[item]}"),
+                subtitle: Text("List 1 subtitle position ${list[item]}"),
+              ),
+              Container(
+                color: Colors.cyan,
+                height: 400,
+                child: ListView.separated(
+                    separatorBuilder: (context, it)=> Divider(
+                      color: Colors.pink,
+                    ),
+                    itemCount: list.length,
+                    // scrollDirection: Axis.horizontal,
+                    itemBuilder: (c, it) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text("List2 position ${list[it]}"),
+                            subtitle: Text("List2 subtitle position ${list[it]}"),
+                            trailing: Icon(Icons.add),
+                            leading: Container(
+                              height: 50,
+                              width: 50,
+                              decoration:
+                              BoxDecoration(shape: BoxShape.circle, color: Colors.yellow),
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+              )
+            ],
+          );
+        });
+  }
+}
